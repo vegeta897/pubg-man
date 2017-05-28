7e31881fc538bcf91d3e815a7000fe016d773e3a
Devin Spikowski <vegeta897@gmail.com>
2017-05-28 17:44:18 -0400

    Restart with create-react-app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
-'use strict';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/main.css';
-import App from './components/App.jsx';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
