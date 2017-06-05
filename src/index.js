40acc682bc1f3a9a568bb2bfea09126f8b02a549
Devin Spikowski <vegeta897@gmail.com>
2017-06-05 19:37:31 -0400

    Replace semantic-ui CDN link with module import, add eslint file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import Store from './store';
