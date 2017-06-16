ad590dd6999e87ab55df98154eaabf637c11f063
Devin Spikowski <vegeta897@gmail.com>
2017-06-16 01:29:15 -0400

    Use inline svg instead of img element

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card } from 'semantic-ui-react';
+import map from './../config/map.json';
 
 class Map extends Component {
     render() {
@@ -7,7 +8,18 @@ class Map extends Component {
             <Card fluid className='Map'>
                 <Card.Content>
                     <Card.Header content="Map" />
-                    <Card.Description content={<Image src="map.svg" />} />
+                    <Card.Description>
+                        <Card.Content>
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+                                 preserveAspectRatio="none" width="100%" viewBox="0 0 1016 1016">
+                                <path fill="#aaa" stroke="none" d={map.data} />
+                                <circle cx="375" cy="360" r="300"
+                                        style={{ fill: 'none', stroke: 'blue', strokeWidth: '3' }} />
+                                <g transform="matrix( 1, 0, 0, 1, 0,0) ">
+                                </g>
+                            </svg>
+                        </Card.Content>
+                    </Card.Description>
                 </Card.Content>
             </Card>
         );
